5b7070b4e86b363e571686a0bccacfd528117d89
Singh <parikshit.k.singh@accenture.com>
2018-05-22 10:49:02 +0530

    commit hello program

---
+++ DemoClass.java
@@ -0,0 +1,18 @@
+package demoPackage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class DemoClass
+{
+
+   public static void main(String[] args)
+   {
+       System.out.println("Hello");
+   }
+
+}
