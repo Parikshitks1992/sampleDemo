c911bbd9ecb3ef211b702dfb6db671e091e37219
Parikshitks1992 <39510854+Parikshitks1992@users.noreply.github.com>
2018-05-23 19:02:57 +0530

    Update DemoClass.java

--- src/test/java/demoPackage/DemoClass.java
+++ src/test/java/demoPackage/DemoClass.java
@@ -1,12 +1,5 @@
 package demoPackage;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Optional;
-import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
-
 public class DemoClass
 {
 
